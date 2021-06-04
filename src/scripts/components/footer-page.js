15347dd7e7eb5cabf30fa0369429ff4266e14e77
narashliwldn <nahdiasholihah@gmail.com>
2021-06-05 00:01:04 +0700

    navbarnya sudah jadi tinggal footernya yang belum

--- src/scripts/components/footer-page.js
+++ src/scripts/components/footer-page.js
@@ -10,17 +10,7 @@ class FooterPage extends HTMLElement {
 
   render(){
     this.innerHTML = `
-    <style>
-      footer {
-        background-color: #b9d7ea;
-        color: white;
-        padding: 1em;
-        width: 100%;
-        text-align: center;
-      }
-
-    </style>
-    <footer>
+    <footer class="footer">
       <p>copyright 2020 - Restopedia</p>
     </footer>
     `;
