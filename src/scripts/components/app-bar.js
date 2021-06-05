d28bd7a948abaa7e71cee4e1765098abf006d7d0
narashliwldn <nahdiasholihah@gmail.com>
2021-06-05 11:26:04 +0700

    drawer nav masih berantakan

--- src/scripts/components/app-bar.js
+++ src/scripts/components/app-bar.js
@@ -10,13 +10,13 @@ class AppBar extends HTMLElement {
 
   render(){
     this.innerHTML = `
-    <nav class="navbar" id="drawer">
+    <nav class="navbar">
       <div class="header">
         <h1 class="header_title">
           Restopedia
         </h1>
       </div>
-        <ul class="nav-list">
+        <ul class="nav-list" id="drawer">
           <li class="nav-item"><a href="#">Home</a></li>
           <li class="nav-item"><a href="#">Favorite</a></li>
           <li class="nav-item"><a href="#">About Us</a></li>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,3 +11,18 @@ import './components/resto-list.js'
 import './data/data-source.js';
 
 // document.addEventListener("DOMContentLouder", main);
+
+const menuButtonElement = document.querySelector("#menu");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main");
+
+menuButtonElement.addEventListener("click", event => {
+ drawerElement.classList.toggle("open");
+ event.stopPropagation();
+});
+
+
+mainElement.addEventListener("click", event => {
+ drawerElement.classList.remove("open");
+ event.stopPropagation();
+})
